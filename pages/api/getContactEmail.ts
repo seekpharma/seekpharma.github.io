b8760cbd886f9280379061e147323e976ccc8359
Kenan Hacan <knnhcn@gmail.com>
2022-07-07 13:52:05 +0200

    wip

--- pages/api/getContactEmail.ts
+++ pages/api/getContactEmail.ts
@@ -1,5 +1,3 @@
-import { NextRequest } from "next/server";
-
-export default function handler(req: NextRequest, res: any) {
+export default function handler(req: any, res: any) {
   res.status(200).json({ email: "info@seekpharma.com" });
 }
