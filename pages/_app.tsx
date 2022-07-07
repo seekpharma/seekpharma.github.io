9c8b7c159e25177b24a5d811941b2ac1af7bf368
Kenan Hacan <knnhcn@gmail.com>
2022-07-07 23:00:15 +0200

    move i18n to root

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { I18nextProvider } from "react-i18next";
-import i18n from "./i18n/i18n";
+import i18n from "../i18n/i18n";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
